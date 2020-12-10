0b77a2f18c3a725de5cd5ee040c706aa1d048f11
erick <erick.gunawan70@gmail.com>
2020-12-11 00:41:35 +0700

    added players to game room

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="home">
+    <nav class="navbar navbar-light bg-light">
+      <div class="container d-flex justify-content-between">
+        <div v-for="(player,i) in players" :key="i"  class="p-2">{{ player.name }} => {{ player.score }}</div>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'Home'
+  name: 'Home',
+  computed: {
+    players () {
+      return this.$store.state.players
+    }
+  }
 }
 </script>
 
