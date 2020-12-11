9a5a278ca55b79a7bb3ae0b179d61912565497bc
tiorzal <tioralmunazir@gmail.com>
2020-12-11 14:48:12 +0700

    setup deploy

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import VueSocketIO from 'vue-socket.io'
 
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: 'http://localhost:3000/',
+  connection: 'https://point-blank-gp14.herokuapp.com/',
   vuex: { // untuk mengkoneksikan ke vuex
     store,
     actionPrefix: 'SOCKET_',
